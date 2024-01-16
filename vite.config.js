89ce657c7d79939dec8a609ef6137572516db76f
Muzamil Ahmad <66633369+Muzamil-07@users.noreply.github.com>
2024-01-16 15:59:05 +0500

    done

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
-export default defineConfig(({ mode }) => {
-  const base_url =
-    mode === 'development' ? 'http://localhost:3000' : 'http://strikeo.com'
-  return {
-    plugins: [react()],
-    build: {
-      outDir: 'dist',
-      assetsDir: 'assets',
-      emptyOutDir: true
-    },
-    resolve: {
-      alias: {
-        '@': resolve(__dirname, './src')
-      }
-    },
-    server: {
-      proxy: {
-        '/api': {
-          target: base_url,
-          changeOrigin: true
-        }
-      }
-    }
-  }
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()]
 })
